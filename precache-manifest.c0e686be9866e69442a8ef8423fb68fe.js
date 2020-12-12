b758f562894214a8bd5a5a3eac3cc6dcd43966e1
Thanh <Trantienthanh2412@gmail.com>
2020-12-12 20:23:38 +0700

    master

---
+++ precache-manifest.c0e686be9866e69442a8ef8423fb68fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60264cf21eafa5372d88e4a708c59194",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "e997b4f50d30330a0979",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "056ad1615c66b973fffd",
+    "url": "/material-dashboard-react/static/css/main.6e4002a3.chunk.css"
+  },
+  {
+    "revision": "e997b4f50d30330a0979",
+    "url": "/material-dashboard-react/static/js/2.659129c5.chunk.js"
+  },
+  {
+    "revision": "2102b888ba48ed39b2f1cef3241475e3",
+    "url": "/material-dashboard-react/static/js/2.659129c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056ad1615c66b973fffd",
+    "url": "/material-dashboard-react/static/js/main.1f4fdc0d.chunk.js"
+  },
+  {
+    "revision": "a8a8dcd4eb50f7154de3",
+    "url": "/material-dashboard-react/static/js/runtime-main.cb7d5160.js"
+  },
+  {
+    "revision": "29a432349781e6c06a74097b6b0049d6",
+    "url": "/material-dashboard-react/static/media/image_pesonal.29a43234.jpg"
+  },
+  {
+    "revision": "9a545023273d402581a97120966bc87d",
+    "url": "/material-dashboard-react/static/media/sidebar-1.9a545023.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
